2de6f3650a90976296c62620020c3d79b8d44728
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-04-17 18:46:31 +0200

    move to WeatherActivity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button checkWeatherBTN= findViewById(R.id.checkWeatherBTN);
+        checkWeatherBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,WeatherActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
