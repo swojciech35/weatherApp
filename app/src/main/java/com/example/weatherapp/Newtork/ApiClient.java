ac6d3c33d1eadb426a985c97d08031a3e03d0d6b
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-04-20 21:49:01 +0200

    add function to get data from internet

--- app/src/main/java/com/example/weatherapp/Newtork/ApiClient.java
+++ app/src/main/java/com/example/weatherapp/Newtork/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     private static Retrofit retrofit=null;
 
-    static Retrofit getClient(){
+    public static Retrofit getClient(){
         if(retrofit==null){
             retrofit=new Retrofit.Builder()
                     .baseUrl("https://api.openweathermap.org/data/2.5/")
--- app/src/main/java/com/example/weatherapp/ViewModel/WeatherViewModel.java
+++ app/src/main/java/com/example/weatherapp/ViewModel/WeatherViewModel.java
@@ -1,29 +1,50 @@
 package com.example.weatherapp.ViewModel;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.weatherapp.Model.Main;
 import com.example.weatherapp.Model.WeatherModel;
+import com.example.weatherapp.Newtork.ApiClient;
+import com.example.weatherapp.Newtork.WeatherApi;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WeatherViewModel extends ViewModel {
     private MutableLiveData<WeatherModel> weather = new MutableLiveData<>();
 
     public WeatherViewModel() {
-        WeatherModel weatherModel = new WeatherModel();
-        weatherModel.setName("Warszawa");
-        Main main = new Main();
-        main.setTemp("20");
-        main.setHumidity("50");
-        main.setPressure("1024");
-        main.setTemp_min("10");
-        main.setTemp_max("21");
-        weatherModel.setMain(main);
-        weather.setValue(weatherModel);
     }
 
     public LiveData<WeatherModel> getWeatherLifeData() {
         return weather;
+    }
+
+    public void getWeatherData(String city) {
+        WeatherApi weatherApi = ApiClient.getClient().create(WeatherApi.class);
+        Call<WeatherModel> call = weatherApi.getWeatherData(city);
+
+        call.enqueue(new Callback<WeatherModel>() {
+            @Override
+            public void onResponse(Call<WeatherModel> call, Response<WeatherModel> response) {
+                if (response.isSuccessful()) {
+                    WeatherModel weatherModel = response.body();
+                    weather.setValue(weatherModel);
+                    Log.i("Get Data", "get data successful");
+                } else {
+                    Log.e("Get Data", "response is not successful");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeatherModel> call, Throwable t) {
+                Log.e("Get Data", "response is not successful");
+            }
+        });
+
     }
 }
